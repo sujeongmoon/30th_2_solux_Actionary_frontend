0a24da76138be4a34610705b30b59e47ae0ca154
LINAGG1 <leeng060124@gmail.com>
2026-01-09 17:53:46 +0900

    [Feat]: 캘린더, 투두리스트 영역

--- src/pages/TodoListPage/TodoListPage.tsx
+++ src/pages/TodoListPage/TodoListPage.tsx
@@ -5,10 +5,10 @@ import './TodoListPage.css'
 import { useTodos } from '../../hooks/useTodos';
 import { useTodoCategories } from '../../hooks/useTodoCategories';
 import ddd from "../../assets/TodoList/ddd.svg";
-import type { CreateTodoRequest, Todo } from '../../api/Todos/todosApi';
+import type { CreateTodoRequest } from '../../api/Todos/todosApi';
 
 const TodoListPage: React.FC = () => {
-  const { selectedDate, setSelectedDate, todos, loading, addTodo, editTodo, changeStatus, removeTodo, toggleRoutine} = useTodos();
+  const { todos, loading, addTodo, editTodo, changeStatus, removeTodo } = useTodos();
   const { categories } = useTodoCategories();
 
   // 상태 관리
@@ -17,19 +17,36 @@ const TodoListPage: React.FC = () => {
   const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
   const [editingText, setEditingText] = useState("");
 
+  const today = new Date();
+  const todayString = today.toISOString().split("T")[0];
+  const [selectedDate, setSelectedDate] = useState<string>(todayString);
+
 
   // 날짜 클릭
-  const handleDateChange = (value: any) => {
-    let date: Date;
-    if (Array.isArray(value)) {
-      if (!value[0]) return;
-      date = value[0];
-    } else {
-      if (!value) return;
-      date = value;
-    }
-    setSelectedDate(date.toISOString().split("T")[0]);
-  };
+const handleDateChange = (value: any) => {
+  if (!value) return;
+
+  const date: Date = Array.isArray(value) ? value[0] : value;
+  if (!(date instanceof Date)) return;
+
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  setSelectedDate(`${year}-${month}-${day}`);
+};
+
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const dayNames = ["일요일", "월요일" , "화요일", "수요일", "목요일", "금요일", "토요일"];
+    const dayName = dayNames[date.getDay()];
+    
+    return `${year} / ${month} / ${day} / ${dayName}`;
+  }
   
 
   // 투두 생성
@@ -48,17 +65,23 @@ const TodoListPage: React.FC = () => {
 
   return (
     <div className="todo-page">
-    
       <div className="calendar-container">
         <Calendar
           onChange={handleDateChange}
           value={selectedDate ? new Date(selectedDate) : new Date()}
+          locale="ko-KR"
+          calendarType="gregory"
+          prev2Label={null}
+          next2Label={null}
+
+          formatDay={(_,date) => date.getDate().toString()}
         />
     </div>
 
     <div className="todo-container">
       <div className="todo-header">
-        <span>{selectedDate}</span>
+        <span>{selectedDate ? formatDate(selectedDate) : ""}</span>
+        <div className="category-dropdown-wrapper">
         <button
           className="category-dropdown-button"
           onClick={() => setCategoryDropdownOpen(prev => !prev)}
@@ -71,6 +94,7 @@ const TodoListPage: React.FC = () => {
             <li> 카테고리 관리</li>
           </ul>
         )}
+        </div>
       </div>
 
       <div className="categories-list">
@@ -110,11 +134,11 @@ const TodoListPage: React.FC = () => {
 
                     <div className="btn-group">
                       <button
-                        className={`status-btn success ${todo.status === 'DONE' ? 'active' : ''}`}
+                        className={`todo-status-btn success ${todo.status === 'DONE' ? 'active' : ''}`}
                         onClick={() => changeStatus(todo.todoId,'DONE')}
                       > 달성 </button>
                       <button
-                        className={`status-btn fail ${todo.status === 'FAILED' ? 'active' : ''}`}
+                        className={`todo-status-btn fail ${todo.status === 'FAILED' ? 'active' : ''}`}
                         onClick={() => changeStatus(todo.todoId, 'FAILED')}
                       >
                         실패
@@ -134,9 +158,6 @@ const TodoListPage: React.FC = () => {
                             수정
                           </li>
                           <li onClick={() => removeTodo(todo.todoId)}>삭제</li>
-                          <li onClick={() => toggleRoutine(todo)}>
-                            {todo.routineId ? "루틴 해제" : "루틴 생성"}
-                          </li>
                         </ul>
                       )}
                     </div>
