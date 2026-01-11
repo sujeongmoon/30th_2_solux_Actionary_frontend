a091107d494d03355ebdf3f26cf77528ac1074aa
gain-0525 <kimgain0525@sookmyung.ac.kr>
2026-01-11 12:57:07 +0900

    [Fix]: 전체 검색용 코드

--- src/pages/SearchPage/AllSearch.tsx
+++ src/pages/SearchPage/AllSearch.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { searchAll } from '../../api/Search/SearchAll';
+import type { SearchStudyItemComponent } from '../../api/Search/SearchStudy';
+import type { SearchPostItem } from '../../api/Search/SearchPost';
+import StudySearchSection from '../../components/Search/StudySearchSection';
 import PostSearchSection from '../../components/Search/PostSearchSection';
-import StudySearchSection from "../../components/Search/StudySearchSection";
+
 
 const AllSearch: React.FC = () => {
+  const [params] = useSearchParams();
+  const keyword = params.get('keyword') ?? '';
+
+  //const [studies, setStudies] = useState<SearchStudyItemComponent[]>([]);
+  const [posts, setPosts] = useState<SearchPostItem[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!keyword) return;
+
+    const fetchSearch = async () => {
+      try {
+        setIsLoading(true);
+        const res = await searchAll(keyword);
+        const data = res.data.data ?? res.data;
+        setStudies(res.data ?? []);
+        setPosts(data.posts ?? []);
+      } catch (e) {
+        console.error('전체 검색 실패', e);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchSearch();
+  }, [keyword]);
+
   return (
     <div>
-      <PostSearchSection />
       <StudySearchSection />
+      {!isLoading && <PostSearchSection posts={posts} />}
     </div>
   );
 };
