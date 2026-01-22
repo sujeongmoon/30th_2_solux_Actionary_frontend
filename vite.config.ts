933372010f888c7a68cc104f499aa392c5522c26
peisonger <realsonge@sookmyung.ac.kr>
2026-01-22 20:48:46 +0900

    [Fix] 퇴장 새로고침 처리 및 상대경로

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // 외부 접속 허용
-    port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://13.209.205.33:8080/', // EC2 내부 도커 백엔드 주소
+      '/ws': {
+        target: 'actionary.site', 
         changeOrigin: true,
-        secure: false,
+        ws: true
       },
-    },
+      '/api': {
+        target: 'actionary.site',
+        changeOrigin: true,
+      }
+    }
   },
 });
